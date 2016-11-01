91e03fd899a5c5e415c1483832d39e1214741a47
yurcash25 <yurcash@yahoo.ca>
2016-11-01 04:50:04 -0400

    File was updated

--- inheritance.php
+++ inheritance.php
@@ -2,6 +2,30 @@
 
 class User {
 
-private $username;
+protected $username;
+  
+public function setUserName ($name){
+  
+  $this -> username = $name;
+}
+public function getUsername()
+{
+  return $this->userName;
+}
 
 }
+class Admin extends User {
+  
+  public function expressYourRoll()
+  {
+   return "Admin"; //Another option: return strtolower(__CLASS__);
+  }
+  public function sayHello(){
+    
+    return "Hello". strtolower(__CLASS__)." ,".$this->getUsername();
+  }  
+}
+$admin1 = new Admin();
+$admin1->setUserName("Balthazar");
+echo $admin1->sayHello();
+//echo "Hello ".$admin1->expressYourRoll().", ".$admin1->getUsername();
