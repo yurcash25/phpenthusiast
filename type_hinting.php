02f47ad94f71a353a2e351d5a59f2d42b00bacc2
yurcash25 <yurcash@yahoo.ca>
2016-11-05 01:48:39 -0400

    updated.File is complete.

--- type_hinting.php
+++ type_hinting.php
@@ -17,10 +17,40 @@ calcNumMilesOnFullTank($models);
 
 class Car {
 
-protected driver;
+protected $driver;
+protected $model;
+protected $hasSunRoof;
+protected $numberOfDoors;
+protected $price;
+
 //the constructor can only get Driver object as argument
 public function __construct( Car $driver)
 {
+ $this->driver=$driver;
+}
+//string type hinting
+public function setModel(string $model)
+{
+$this->model=$model;
+}
+//boolean type hint
+public function setHasSunRoof(bool $value)
+{
+$this->hasSunRoof=$value;
+}
+//integer type hint
+public function setNumberOfDoors (int $value)
+{
+$this->numberOfDoors = $value;
+}
+//float type hint
+public function setPrice (float $value)
+{
+ $this->price=$value;
+}
 
 }
-}
+class Driver {}
+
+$driver1 = new Driver();
+$car1 = new Car ($driver1);
